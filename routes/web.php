f14686e58de73f967853f8074d1de5d49de947aa
marius <marius.paduraru@gmail.com>
2024-05-20 23:36:22 +0100

    Update routes and add new ones
    The commit updates the default route to return 'Main Page' instead of the welcome view. It also adds new routes to handle requests to '/about' and '/test/{id}'. Furthermore, a redirect route '/tst' is added which redirects to the 'test' route with an id of 1.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('Main Page');
+});
+
+Route::get('/about', function () {
+    return ('About Page');
+})->name('about');
+
+Route::get('/test/{id}', function (int $id) {
+    return ('Test page with id: ' . $id);
+})->name('test');
+
+Route::get('/tst', function () {
+    return redirect()->route('test', ['id' => 1]);
 });
