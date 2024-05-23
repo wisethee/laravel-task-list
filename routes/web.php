1b09db41163c4d0840d1b079f8237599077c49e5
marius <marius.paduraru@gmail.com>
2024-05-23 22:56:30 +0100

    Refactor Task model usage in routes
    Updated the routing code to use the Task model from the App\Models namespace. This change includes utilizing queries to fetch tasks rather than working with the hardcoded array of tasks. The Task class definition and array of tasks have been removed from the routing file. Instead, we are directly querying the database for tasks using the Task model's methods.

--- routes/web.php
+++ routes/web.php
@@ -1,80 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use App\Models\Task;
 
-class Task
-{
-    public function __construct(
-        public int     $id,
-        public string  $title,
-        public string  $description,
-        public ?string $long_description,
-        public bool    $completed,
-        public string  $created_at,
-        public string  $updated_at
-    )
-    {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks,
+        'tasks' => Task::all(),
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
-
-    if (!$task) {
-        abort(ResponseAlias::HTTP_NOT_FOUND);
-    }
-    
+Route::get('/tasks/{id}', function ($id) {
     return view('show', [
-        'task' => $task,
+        'task' => Task::findOrFail($id),
     ]);
 })->name('tasks.show');
